08ba68c035b553441676977318fbfadf9bcdc5e8
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-01-09 02:24:21 -0500

    Add hidden bookmark URL to incorrect password page

--- index.js
+++ index.js
@@ -64,6 +64,10 @@ async function main() {
       // Set the "decrypt without redirect" URL appropriately
       document.querySelector("#no-redirect").href =
         `https://jstrieb.github.io/link-lock/decrypt/#${hash}`;
+
+      // Set the "create hidden bookmark" URL appropriately
+      document.querySelector("#hidden").href =
+        `https://jstrieb.github.io/link-lock/hidden/#${hash}`;
       return;
     }
 
